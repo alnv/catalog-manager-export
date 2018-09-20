99b812325a1ecceb45e3096d48364a8ccfea5764
Alexander Naumov <me@alexandernaumov.de>
2018-09-20 14:59:31 +0200

    add labels

---
+++ src/Resources/contao/languages/de/tl_catalog.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog']['export_legend'] = 'Export-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog']['useExport'] = [ 'Export erlauben', 'Hier können Sie das Export Modul für diesen Katalog aktivieren.' ];
---
+++ src/Resources/contao/languages/de/tl_catalog_export.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_export']['new'] = [ 'Export erstellen', 'Hier können Sie einen neuen Export erstellen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['edit'] = [ '', '' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['delete'] = [ '', '' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['show'] = [ '', '' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_export']['name'] = [ 'Name', 'Bitte geben Sie eine Export Bezeichnung ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['type'] = [ 'Typ', 'Bitte wählen Sie einen Export Typ aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['includeHeader'] = [ 'Kopfzeile hinzufügen', 'Hier können Sie die Kopfzeile hinzufügen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['parser'] = [ 'Werte formatieren', 'Hier können Sie die Werte formatieren.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['table'] = [ 'Tabelle', 'Dieser Wert wird automatisch ermittelt.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['match'] = [ 'Filter', 'Hier können Sie Ihre Daten Auswahl begrenzen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['order'] = [ 'Reihenfolge', 'Hier können Sie eine Reihenfolge festlegen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['limit'] = [ 'Limit', '' ];
+$GLOBALS['TL_LANG']['tl_catalog_export']['offset'] = [ 'Überspringen', '' ];
