51d3f6c5041f8754256614227e18c83fde6ca10d
Alexander Naumov <me@alexandernaumov.de>
2018-09-20 15:00:53 +0200

    add export labels

---
+++ src/Resources/contao/dca/tl_catalog.php
@@ -0,0 +1,17 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_catalog']['palettes']['default'] .= ';{export_legend:hide},useExport';
+
+$GLOBALS['TL_DCA']['tl_catalog']['fields']['useExport'] = [
+
+    'label' => &$GLOBALS['TL_LANG']['tl_catalog']['useExport'],
+    'inputType' => 'checkbox',
+
+    'eval' => [
+
+        'tl_class' => 'clr',
+    ],
+
+    'exclude' => true,
+    'sql' => "char(1) NOT NULL default ''"
+];
