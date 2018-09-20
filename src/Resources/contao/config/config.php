4937bb8c5a95b8b59362777ff1d0a8f8f0dfb019
Alexander Naumov <me@alexandernaumov.de>
2018-09-20 14:59:09 +0200

    add hooks

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,4 @@
 <?php
 
-//
+$GLOBALS['TL_HOOKS']['catalogManagerModifyBackendModule'][] = [ 'CatalogManager\ExportBundle\Library\Hooks', 'modifyBackendModule' ];
+$GLOBALS['TL_HOOKS']['loadDataContainer'][] = [ 'export.library.hooks', 'setExport' ];
