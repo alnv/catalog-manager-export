5f5b2972a269847f18e2983f219fad4c3a75a9a5
Alexander Naumov <me@alexandernaumov.de>
2018-09-21 10:15:07 +0200

    add missing labels

--- src/Resources/contao/languages/de/tl_catalog.php
+++ src/Resources/contao/languages/de/tl_catalog.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_catalog']['export_legend'] = 'Export-Einstellungen';
-$GLOBALS['TL_LANG']['tl_catalog']['useExport'] = [ 'Export erlauben', 'Hier können Sie das Export Modul für diesen Katalog aktivieren.' ];
+$GLOBALS['TL_LANG']['tl_catalog']['useExport'] = [ 'Export erlauben', 'Hier könne Sie für dieses Modul die Export Erweiterung aktivieren.' ];
